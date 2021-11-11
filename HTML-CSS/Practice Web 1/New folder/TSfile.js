711d48b56d7291b8fabb9f7a4f3473e4c1c6f05e
Prasad <prasad.patil.shiva@gmail.com>
2021-11-11 12:04:23 +0530

    Adding all files for tracking progress

---
+++ HTML-CSS/Practice Web 1/New folder/TSfile.js
@@ -0,0 +1,5 @@
+function func(a, b) {
+    console.log("Prasad is coder");
+    return a + b;
+}
+func(6, 8);
---
+++ HTML-CSS/Practice Web 1/New folder/TSfile.ts
@@ -0,0 +1,7 @@
+//Sample Function
+function func(a:number, b:number){
+    console.log("Prasad is coder");
+    return a+b;
+}
+
+func(6,8);
